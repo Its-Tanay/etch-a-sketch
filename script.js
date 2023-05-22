eea68ff233eb7c0eb781f70f153be5bb57da5164
Its-Tanay <tantiw1910@gmail.com>
2023-05-22 16:12:24 +0530

    Finished scripting basic functionality of the game. Still need to add a few more features and make it look better.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const slider1 = document.querySelector('#slider1');
 const sliderValue1 = document.querySelector('#sliderValue1');
 const container = document.querySelector('.container');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+const btn4 = document.querySelector('#btn4');
 
 const grid = document.createElement('div');
 grid.setAttribute('class', 'grid');
@@ -30,14 +33,38 @@ function createGrid(n){
         blocks.style.width = `${blockSize}px`;
         blocks.style.height = `${blockSize}px`;
 
-        console.log(blockSize);
-        grid.appendChild(blocks); 
+        btn3.addEventListener('click', function() {
+            changeColour(blocks);
+        });
+
+        btn4.addEventListener('click', function() { 
+            eraser(blocks);
+        });
+
+        reset(blocks);
+
+        grid.appendChild(blocks);     
     }
     container.appendChild(grid);
 }
 
+function changeColour(e){
+    e.addEventListener('mouseover', function(){
+        e.style.backgroundColor = 'black';
+    });
+}
 
+function reset(e){
+    btn2.addEventListener('click', function(){
+        e.style.backgroundColor = 'white';
+    });
+}
 
+function eraser(e){
+    e.addEventListener('mouseover', function(){
+        e.style.backgroundColor = 'white';
+    });
+}
 
 
 
